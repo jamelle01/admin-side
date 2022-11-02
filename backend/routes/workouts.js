309a8b1ed77f129fc7d733af9d2ebcd095847884
Ell do <91648854+jamelle01@users.noreply.github.com>
2022-11-02 17:46:38 +0800

    done backend

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,29 +1,28 @@
 const express = require('express');
+const Workout = require('../models/workoutModel');
 
 const router = express.Router();
+const {
+    getWorkouts, 
+    getWorkout, 
+    createWorkout, 
+    deleteWorkout, 
+    updateWorkout
+  } = require('../controllers/workoutController');
 
-// GET all workouts
-router.get('/', (req, res) => {
-    res.json({mssg: 'GET all workouts'})
-})
+// GET all 
+router.get('/', getWorkouts);
 
-// GET single workout
-router.get('/:id', (req, res) => {
-    res.json({mssg: 'GET a single workout'})
-})
+// GET single 
+router.get('/:id', getWorkout);
 
 // POST a new workout
-router.post('/', (req, res) => {
-    res.json({mssg: 'POST a new workout'})
-})
+router.post('/', createWorkout);
 
 // DELETE a workout
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'DELETE a workout'})
-})
+router.delete('/:id', deleteWorkout);
 
 // PATCH a workout 
-router.patch('/:id', (req, res) => {
-    res.json({mssg: 'UPDATE a workout'})
-})
+router.patch('/:id', updateWorkout);
+
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 
 const express = require ('express');
-const workoutRoutes = require('./routes/workouts')
+const workoutRoutes = require('./routes/workouts');
+const mongoose = require('mongoose');
 
 //express app
 const app = express();
@@ -15,10 +16,19 @@ app.use((req, res, next) =>{
 })
 
 //routes
-app.use('/api/workouts', workoutRoutes)
+app.use('/api/workouts', workoutRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () =>{
-    console.log("listening on port 4000!!!");
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () =>{
+            console.log("connected to db and listening on port 4000!!!");
+        })
+    })
+    .catch((error) => {
+        console.log("sheessh"+ error);
+    })
+
+
 
