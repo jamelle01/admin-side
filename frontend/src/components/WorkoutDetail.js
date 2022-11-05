b58d547f1693de3e1f21026957ba0467d14ed9a0
Ell do <91648854+jamelle01@users.noreply.github.com>
2022-11-06 00:50:29 +0800

    going with edit

--- frontend/src/components/WorkoutDetail.js
+++ frontend/src/components/WorkoutDetail.js
@@ -22,7 +22,8 @@ const WorkoutDetails = ({ workout }) => {
       <p><strong>Load (kg): </strong>{workout.load}</p>
       <p><strong>Number of reps: </strong>{workout.reps}</p>
       <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
-      <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+      <span class="material-symbols-sharp edit">edit</span>
+      <span className="material-symbols-sharp delete" onClick={handleClick}>delete</span>
     </div>
       
     );
